59561cb87e38b3fc51ba3cdf0ceb5c4215ba2648
vishnusankar55 <vishnusankar55@gmail.com>
2022-06-23 14:51:40 +0530

    updated api.py file and updated Readme file

--- api.py
+++ api.py
@@ -8,8 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def demo():
     return "Hello "
+@app.route("/test", methods=["GET"])
+def index():
+    print(f"Inside flask function: {threading.current_thread().name}")
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(hello())
+    return jsonify({"result": result})
+
+async def hello():
+    await asyncio.sleep(5)
+    return 1       
 
 
 if __name__ == "__main__":
     app.run (debug = True)   
-    
